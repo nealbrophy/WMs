8fb6834efc369f4ddda3cb2f812bf46d6ddee9a3
Neal Brophy <nbrophy@pm.me>
2020-08-18 09:24:52 +0100

    Adjust powermenu, config, i3blocks

--- MX-i3/rofi/scripts/powermenu.sh
+++ MX-i3/rofi/scripts/powermenu.sh
@@ -4,16 +4,13 @@
 option0="lock"
 option1="logout"
 option2="suspend"
-option3="scheduled suspend (10min)"
-option4="scheduled suspend (20min)"
-option5="scheduled suspend (30min)"
-option6="reboot"
-option7="shutdown"
+option3="reboot"
+option4="shutdown"
 
 # options passed into variable
-options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7"
+options="$option0\n$option1\n$option2\n$option3\n$option4"
 
-chosen="$(echo -e "$options" | rofi -lines 8 -dmenu -p "power")"
+chosen="$(echo -e "$options" | rofi -lines 5 -dmenu -p "power")"
 case $chosen in
     $option0)
         blurlock;;
@@ -21,14 +18,8 @@ case $chosen in
         i3-msg exit;;
     $option2)
          pm-suspend;;
-	$option3)
-		sleep 600 && pm-suspend;;
+    $option3)
+        sudo reboot;;
 	$option4)
-		sleep 1200 && pm-suspend;;
-	$option5)
-		sleep 1800 && pm-suspend;;
-    $option6)
-        pm-reboot;;
-	$option7)
-        pm-poweroff;;
+        sudo poweroff;;
 esac
